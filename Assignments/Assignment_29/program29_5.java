d4c3789acf2ca7a2c485fbf2521862507e3f107f
AshwiniKauthale <kauthaleashwini40@gmail.com>
2025-11-21 11:11:51 +0530

    Add files via upload

---
+++ Assignments/Assignment_29/program29_5.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow,int iCol)
+    {
+        int i = 0, j = 0;
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = i; j < iCol+i; j++)
+            {
+                System.out.print(j+"\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+
+class program29_5
+{
+    public static void main(String A[])
+    {
+        int iValue1 = 0, iValue2 = 0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of columns :");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1,iValue2);
+    }
+}
