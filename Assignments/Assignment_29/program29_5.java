5cff743532bd0e24778125cd136d1d79713d7db7
AshwiniKauthale <kauthaleashwini40@gmail.com>
2025-11-23 19:44:30 +0530

    Update program29_5.java

--- Assignments/Assignment_29/program29_5.java
+++ Assignments/Assignment_29/program29_5.java
@@ -1,8 +1,31 @@
+///////////////////////////////////////////////////////////////////
+//
+//  Required Packages
+//
+////////////////////////////////////////////////////////////////////
+
 import java.util.*;
+
+///////////////////////////////////////////////////////////////////
+//
+//  Class Name : Pattern
+//
+////////////////////////////////////////////////////////////////////
 
 class Pattern
 {
-    public void Display(int iRow,int iCol)
+////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Diaplay
+//  Description :   It is used to display pattern
+//  Input :         Integer,Integer
+//  Output :        Displays  pattern on screen
+//  Author :        Ashwini Vishnu Kauthale
+//  Date :          20/11/2025
+//
+////////////////////////////////////////////////////////////////////
+
+     public void Display(int iRow,int iCol)
     {
         int i = 0, j = 0;
         for(i = 1; i <= iRow; i++)
@@ -16,21 +39,37 @@ class Pattern
         System.out.println();
     }
 }
+    
+///////////////////////////////////////////////////////////////////
+//
+//  Entry Point Function for Java Application
+//
+///////////////////////////////////////////////////////////////////
 
 class program29_5
 {
     public static void main(String A[])
     {
-        int iValue1 = 0, iValue2 = 0;
+        int iValue1  = 0, iValue2 = 0;
         Scanner sobj = new Scanner(System.in);
 
-        System.out.println("Enter number of rows :");
+        System.out.println("Enter the number of rows :");
         iValue1 = sobj.nextInt();
-
-        System.out.println("Enter number of columns :");
+        System.out.println("Enter the number of columns :");
         iValue2 = sobj.nextInt();
 
         Pattern pobj = new Pattern();
         pobj.Display(iValue1,iValue2);
     }
 }
+
+////////////////////////////////////////////////////////////////////
+//
+//  iRow = 4
+//  iCol = 4
+//  Output : 
+//            1  2  3  4  
+//            2  3  4  5  
+//            3  4  5  6   
+//            4  5  6  7
+////////////////////////////////////////////////////////////////////
